de3f8fd897fb7904ee0ee20de6d1c7388203b964
Rich <rich@ionicsecurity.com>
2016-06-24 11:47:26 -0400

    matched the regex with the cucumber step

--- features/step_definitions/submit_email_reminder_steps.rb
+++ features/step_definitions/submit_email_reminder_steps.rb
@@ -3,7 +3,7 @@ When(/^I add an "(.*?)" reminder$/) do |email|
   @dashboard.shoe_store_home_page.submit_email.click
 end
 
-Then(/^I will see a success notification with my email "(.*?)"$/) do |email|
+Then(/^I will see a success notification with my "(.*?)" displayed$/) do |email|
   Watir::Wait.until(15, 'Success notification element did not appear') { @dashboard.notifications.reminder_email_success.present? }
   @dashboard.notifications.reminder_email_success.text.should eql "Thanks! We will notify you of our new shoes at this email: #{email}"
 end
